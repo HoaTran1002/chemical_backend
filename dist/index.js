ce42ee32d909f8a3b17a2c706c143ef5feccb90c
HoaTran1002 <vanhoa0908751520@gmail.com>
2024-07-16 23:41:48 +0700

    ADD:cors

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
 var index_router_1 = __importDefault(require("./router/index.router"));
 var body_parser_1 = __importDefault(require("body-parser"));
 var cookie_parser_1 = __importDefault(require("cookie-parser"));
@@ -11,6 +12,12 @@ var dotenv_1 = __importDefault(require("dotenv"));
 var errorHandlingMiddleware_1 = require("./middleware/errorHandlingMiddleware");
 dotenv_1.default.config();
 var app = (0, express_1.default)();
+var corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use((0, cors_1.default)(corsOptions));
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use(body_parser_1.default.json());
 app.use((0, cookie_parser_1.default)());
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import useRouter from './router/index.router'
 import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
@@ -6,6 +7,13 @@ import dotenv from 'dotenv'
 import { errorHandlingMiddleware } from '~/middleware/errorHandlingMiddleware'
 dotenv.config()
 const app = express()
+
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
